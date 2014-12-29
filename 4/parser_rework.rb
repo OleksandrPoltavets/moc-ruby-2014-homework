2aff7d7fc93850d285a347332632d791b9f95c37
Oleksandr Poltavets <sancho.ck@gmail.com>
2014-12-29 22:29:05 +0200

    parser-rework-completed

--- 4/parser_rework.rb
+++ 4/parser_rework.rb
@@ -2,11 +2,23 @@ require 'json'
 
 module PersonIdentify
 
+  def gender
+    personal_data["gender"]
+  end
+
+  def name
+    personal_data["name"].capitalize
+  end
+
+  def age
+    personal_data["age"]
+  end
+
   def has_name?
     !personal_data["name"].empty?
   end
 
-  def gender_call(gender)
+  def gender_call
     if gender == 'male'
       %w(he his)
     elsif gender == 'female'
@@ -16,7 +28,7 @@ module PersonIdentify
     end
   end
 
-  def man_woman(gender)
+  def man_woman
     if gender == 'male'
       'man'
     elsif gender == 'female'
@@ -25,15 +37,15 @@ module PersonIdentify
   end
 
   def full_name?
-    name.split(' ').length > 1
+    personal_data["name"].split(' ').length > 1
   end
 
-  def adult?(age)
-    age >= 18 ? 'Yes!' : ' No!'
+  def adult?
+    personal_data["age"] >= 18 ? 'Yes!' : ' No!'
   end
 
-  def have_hobbies?(info)
-    info.length > 0
+  def have_hobbies?
+    additional_info.length > 0
   end
 
   def facebook_account?(path)
@@ -48,6 +60,40 @@ module PersonIdentify
     path.split('/').last
   end
 
+  def socials
+    if social_profiles.length > 0
+      p '========================================================'
+      p "Social profiles: "
+      social_profiles.each do |path|
+        p "- facebook: #{social_id(path)}" if facebook_account?(path)
+        p "- twitter: #{social_id(path)}" if twitter_account?(path)
+      end
+    end
+  end
+
+  def hobbies
+    if additional_info["hobby"]
+      p '========================================================'
+      p 'Hobbies:'
+      additional_info["hobby"].each do |hobby|
+        p "- #{hobby}"
+      end
+    end
+  end
+
+  def pets
+    if additional_info["pets"]
+      p '========================================================'
+      p "Pets quantity (#{additional_info["pets"].length}):"
+      additional_info["pets"].each do |pet|
+        p '..................................'
+        p "- name: #{pet["name"]}"
+        p "- species: #{pet["species"]}"
+      end
+    end
+  end
+
+
 end
 
 RESPONSE = '{"person":{"personal_data":{"name": "John Doe", "gender":"male", "age":56},"social_profiles":["https://www.facebook.com/john.doe","https://twitter.com/JohnDoe"],"additional_info":{"hobby":["pubsurfing","drinking","hiking","travelling", "outsourcing"], "pets":[{"name":"Mittens","species":"Felis silvestris catus"},{"name":"Mister","species":"Scottish"}]}}}'
@@ -55,63 +101,15 @@ RESPONSE = '{"person":{"personal_data":{"name": "John Doe", "gender":"male", "ag
 parsed_data = JSON.parse(RESPONSE)
 if parsed_data.key?("person")
   person_object = Struct.new("Person", *parsed_data["person"].keys.collect(&:to_sym))
-  p person_object
   person = person_object.new(*parsed_data["person"].values)
-  p person
 end
 
 person.extend(PersonIdentify)
 
-p person.personal_data["name"]
-p '------'
-p person.has_name?
-
-# class PersonClass
-#   extend PersonIdentify
-#
-#
-# end
-
-# person = PersonClass.new
-
-# p person.personal_data
-# data = person.parse_data(RESPONSE)
-
-# name = data.personal_data["name"].capitalize
-# calling = person.gender_call(data.personal_data["gender"]).last.capitalize
-# appeal = person.gender_call(data.personal_data["gender"]).first.capitalize
-# age = data.personal_data["age"]
-# gender_alias = person.man_woman(data.personal_data["gender"])
-# hobbies = data.additional_info["hobby"]
-# pets = data.additional_info["pets"]
-
-# p 'Here is the profile of the user we received from server:'
-# p '========================================================'
-# p person.has_name?
-
-# p "#{calling} name is '#{name}', and #{appeal} is #{age} years old #{gender_alias}."
-# p "Does #{person.gender_call(data.personal_data["gender"]).last} adult?: #{person.adult?(data.personal_data["age"])}"
-# if data.social_profiles.length > 0
-#   p '================'
-#   p "Social profiles: "
-#   data.social_profiles.each do |path|
-#     p "- facebook: #{person.social_id(path)}" if person.facebook_account?(path)
-#     p "- twitter: #{person.social_id(path)}" if person.twitter_account?(path)
-#     p "- other social: "
-#   end
-# end
-# if hobbies
-#   p '================'
-#   p 'Hobbies:'
-#   hobbies.each do |hobby|
-#     p "- #{hobby}"
-#   end
-# end
-# if pets
-#   p '==================='
-#   p "Pets quantity (#{pets.length}):"
-#   pets.each do |pet|
-#     p "- name: #{pet["name"]}"
-#     p "- species: #{pet["species"]}"
-#   end
-# end
+p 'Here is the profile of the user we received from server:'
+p '========================================================'
+p "#{person.gender_call.last.capitalize} name is '#{person.name}' and #{person.gender_call.first.capitalize} is #{person.age} years old #{person.man_woman}."
+p "Does #{person.gender_call.first} adult?: #{person.adult?}"
+person.socials
+person.hobbies
+person.pets
