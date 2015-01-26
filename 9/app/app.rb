9af2cba0e38e17d3cc32b7cce8d4bfceaa2de3d7
Oleksandr Poltavets <sancho.ck@gmail.com>
2015-01-27 01:30:14 +0200

    lab9-sinatra-pb-api

---
+++ 9/app/app.rb
@@ -0,0 +1,19 @@
+require 'open-uri'
+
+get '/' do
+  "MOC lab 9 on Sinatra!"
+end
+
+get '/time' do
+  "Current time is: #{Time.now}"
+
+  #https://api.privatbank.ua/p24api/exchange_rates?json&date=01.12.2013
+end
+
+get '/pb' do
+  # response = Net::HTTP.get_response("api.privatbank.ua","/p24api/exchange_rates?json&date=01.12.2013")
+  # puts response.body #this must show the JSON contents
+
+  content = open("https://api.privatbank.ua/p24api/exchange_rates?json&date=01.12.2013").read
+  puts content
+end
