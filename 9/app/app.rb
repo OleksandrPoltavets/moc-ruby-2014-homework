49df4311a403cecdd1d30868d3fe097a0221b547
Oleksandr Poltavets <sancho.ck@gmail.com>
2015-01-27 10:28:48 +0200

    parcing_response

--- 9/app/app.rb
+++ 9/app/app.rb
@@ -1,4 +1,5 @@
 require 'open-uri'
+require 'json'
 
 get '/' do
   "MOC lab 9 on Sinatra!"
@@ -11,9 +12,11 @@ get '/time' do
 end
 
 get '/pb' do
-  # response = Net::HTTP.get_response("api.privatbank.ua","/p24api/exchange_rates?json&date=01.12.2013")
-  # puts response.body #this must show the JSON contents
+  date = RandTime.random.strftime('%d.%m.%Y')
 
-  content = open("https://api.privatbank.ua/p24api/exchange_rates?json&date=01.12.2013").read
-  {currencies: content}.to_json
+  content = open("https://api.privatbank.ua/p24api/exchange_rates?json&date=#{date}").read
+  result = JSON.parse(content)
+
+  # {currencies: result}.to_json
+  { date: result["date"], bank: result["bank"], base_currency: result["baseCurrencyLit"], usd_sale: result["exchangeRate"][8]["saleRate"], usd_purchase: result["exchangeRate"][8]["purchaseRate"]}.to_json
 end
